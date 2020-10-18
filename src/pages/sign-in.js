7b700992a7c5f8bd003129d9f72b72f153effa22
Ks1220 <62043437+Ks1220@users.noreply.github.com>
2020-10-18 17:19:34 +0800

    Move some part into component page

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -6,11 +6,6 @@ const Index = () => {
   return (
     <Columns vCentered>
       <Columns.Column>
-        <p className="is-size-3 has-text-weight-bold has-text-centered title">
-          Homiez
-        </p>
-      </Columns.Column>
-      <Columns.Column>
         <SignIn />
       </Columns.Column>
     </Columns>
