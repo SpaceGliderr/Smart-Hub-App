67efa5091970b6aa346e506147ee7e11a83b3179
Ks1220 <62043437+Ks1220@users.noreply.github.com>
2020-10-18 16:10:03 +0800

    Move Sign Up title to the components

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -6,11 +6,6 @@ const Index = () => {
   return (
     <Columns vCentered>
       <Columns.Column>
-        <p className="is-size-3 has-text-weight-bold">
-          Fill in your personal data, you slut!
-        </p>
-      </Columns.Column>
-      <Columns.Column>
         <SignUp />
       </Columns.Column>
     </Columns>
