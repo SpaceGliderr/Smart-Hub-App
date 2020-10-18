5d1c2ad6134dc62f5ad384f74b0ad1c4f1280ffb
Ks1220 <62043437+Ks1220@users.noreply.github.com>
2020-10-18 21:51:44 +0800

    change some bracket error in style.js

--- src/components/Sign In/styles.js
+++ src/components/Sign In/styles.js
@@ -1,30 +1,31 @@
 import css from "styled-jsx/css";
 
 export default css.global`
-    .homiez-signin {
-      margin-top: 1.5em;
-    }
+  .homiez-signin {
+    margin-top: 1.5em;
+  }
+  .image-signin {
+    width: 400px;
+    height: 400px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .forgot-password {
+    margin-bottom: 0.5em;
+  }
+  .divider {
+    margin: 1em 0;
+  }
+  .google-button {
+    margin-bottom: 0.5em;
+  }
+  .google-icon {
+    margin-right: 0.5em;
+  }
+  @media screen and (max-width: 800px) {
     .image-signin {
-      width: 400px;
-      height: 400px;
-      margin-left: auto;
-      margin-right: auto;
-    }
-    .forgot-password {
-      margin-bottom: 0.5em;
-    }
-    .divider {
-      margin: 1em 0;
-    }
-    .google-button {
-      margin-bottom: 0.5em;
-    }
-    .google-icon {
-      margin-right: 0.5em;
-    }
-    @media screen and (max-width: 800px) {
-      .image-signin {
       width: 248px;
       height: 230px;
     }
+  }
 `;
--- src/components/Sign Up/styles.js
+++ src/components/Sign Up/styles.js
@@ -1,33 +1,34 @@
 import css from "styled-jsx/css";
 
 export default css.global`
-    .homiez-signup {
-      margin-top: 1.5em;
-    }
+  .homiez-signup {
+    margin-top: 1.5em;
+  }
+  .image-signup {
+    width: 400px;
+    height: 400px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .link {
+    width: 100%;
+  }
+  .t-and-c {
+    margin-bottom: 0.5em;
+  }
+  .divider {
+    margin: 1em 0;
+  }
+  .google-button {
+    margin-bottom: 0.5em;
+  }
+  .google-icon {
+    margin-right: 0.5em;
+  }
+  @media screen and (max-width: 800px) {
     .image-signup {
-      width: 400px;
-      height: 400px;
-      margin-left: auto;
-      margin-right: auto;
-    }
-    .link{
-      width: 100%
-    }
-    .t-and-c {
-      margin-bottom: 0.5em;
-    }
-    .divider {
-      margin: 1em 0;
-    }
-    .google-button {
-      margin-bottom: 0.5em;
-    }
-    .google-icon {
-      margin-right: 0.5em;
-    }
-    @media screen and (max-width: 800px) {
-      .image-signup {
       width: 248px;
       height: 244px;
     }
+  }
 `;
