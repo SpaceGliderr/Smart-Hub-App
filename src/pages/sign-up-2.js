bca121c9a2b891461b0a791f46d218279bfe2278
Ks1220 <62043437+Ks1220@users.noreply.github.com>
2020-10-19 16:25:00 +0800

    added sign up 2 pages

---
+++ src/pages/sign-up-2.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Columns } from "react-bulma-components";
+import SignUp2 from "../components/Sign Up 2";
+
+const Index = () => {
+  return (
+    <Columns vCentered>
+      <Columns.Column>
+        <SignUp2 />
+      </Columns.Column>
+    </Columns>
+  );
+};
+
+export default Index;
