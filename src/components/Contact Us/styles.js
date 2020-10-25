e82b14854a1cb9399a8db5f92b3043e6039378a9
Ks1220 <62043437+Ks1220@users.noreply.github.com>
2020-10-25 15:11:31 +0800

    created contact us page

---
+++ src/components/Contact Us/styles.js
@@ -0,0 +1,35 @@
+import css from "styled-jsx/css";
+
+export default css.global`
+  .homiez-contact-us {
+    margin-top: 1.5em;
+  }
+  .top-bar {
+    padding-bottom 0.5em;
+    width: 100%;
+    box-shadow: 0 0.7px #e4e4e4;
+    margin-bottom: 1em;
+  }
+  .back-button {
+    position: absolute;
+    padding-top: 1em;
+    border: none;
+    background: none;
+  }
+  .first-caption {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .contact-us {
+    box-shadow: -1px 1px 5px 2px #e4e4e4;
+    padding: 20px;
+    border: 1px solid #F0F0F0;
+    margin-bottom: 1.5em;
+  }
+`;
