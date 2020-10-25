58ac519ee9f077705389e030133e6ed3c0d91b93
Ks1220 <62043437+Ks1220@users.noreply.github.com>
2020-10-25 11:24:00 +0800

    added FAQ component

---
+++ src/components/FAQ/styles.js
@@ -0,0 +1,43 @@
+import css from "styled-jsx/css";
+
+export default css.global`
+  .homiez-account-details {
+    margin-top: 1.5em;
+  }
+  .top-bar {
+    padding-bottom 0.5em;
+    width: 100%;
+    box-shadow: 0 0.7px #e4e4e4;
+    margin-bottom: 1em;
+  }
+  .back-button {
+    position: absolute;
+    padding-top: 1em;
+    border: none;
+    background: none;
+  }
+  .first-caption {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .section-title {
+    padding-bottom: 10px;
+  }
+  .section-description {
+    margin-bottom: 0.5em;
+    min-height: 55px;
+    border-top: 1px solid #DBDBDB;
+    width: 100%;
+  }
+  .right {
+    margin: 0.7em 0;
+    position: absolute;
+    right: 1.8em;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .message {
+    margin-bottom: 0.5em;
+  }
+`;
